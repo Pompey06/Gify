d91ac9b8ce202cc8ba6dd63ed207166b622e97b9
мак <mak@MacBook-Air-Matvej.local>
2025-02-18 16:14:04 +0100

    WIp

--- src/Components/Spollers.jsx
+++ src/Components/Spollers.jsx
@@ -1,65 +1,33 @@
 import arrow from '../assets/arrow.svg'
 import arrow_active from '../assets/active__arrow.svg'
-
+import { useState } from 'react';
 const Spollers = () => { 
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const toggleSpoiler = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
   return (
     <div className="spoilers">
-        <div className="spoiler__wrapper">
+      {[...Array(5)].map((_, index) => (
+        <div
+          key={index}
+          className={`spoiler__wrapper ${activeIndex === index ? 'active' : ''}`}
+          onClick={() => toggleSpoiler(index)}
+        >
           <div className="spoiler">
             <div className="spoiler__title">
-            Text Text Text Text Text Text Text
+              Text Text Text Text Text Text Text
             </div>
-            <img src={arrow} alt="" className="arrow" />
+            <img src={activeIndex === index ? arrow_active : arrow} alt="" className="arrow" />
           </div>
           <div className="spoiler__content">
             Al support can shorten development cycles by automating or speeding up many steps in the software development process.
           </div>
         </div>
-        <div className="spoiler__wrapper active">
-          <div className="spoiler">
-            <div className="spoiler__title">
-            Text Text Text Text Text Text Text
-            </div>
-            <img src={arrow_active} alt="" className="arrow" />
-          </div>
-          <div className="spoiler__content">
-            Al support can shorten development cycles by automating or speeding up many steps in the software development process.
-          </div>
-        </div>
-        <div className="spoiler__wrapper active">
-          <div className="spoiler">
-            <div className="spoiler__title">
-            Text Text Text Text Text Text Text
-            </div>
-            <img src={arrow_active} alt="" className="arrow" />
-          </div>
-          <div className="spoiler__content">
-            Al support can shorten development cycles by automating or speeding up many steps in the software development process.
-          </div>
-        </div>
-        <div className="spoiler__wrapper active">
-          <div className="spoiler">
-            <div className="spoiler__title">
-            Text Text Text Text Text Text Text
-            </div>
-            <img src={arrow_active} alt="" className="arrow" />
-          </div>
-          <div className="spoiler__content">
-            Al support can shorten development cycles by automating or speeding up many steps in the software development process.
-          </div>
-        </div>
-        <div className="spoiler__wrapper active">
-          <div className="spoiler">
-            <div className="spoiler__title">
-            Text Text Text Text Text Text Text
-            </div>
-            <img src={arrow_active} alt="" className="arrow" />
-          </div>
-          <div className="spoiler__content">
-            Al support can shorten development cycles by automating or speeding up many steps in the software development process.
-          </div>
-        </div>
-      </div>
+      ))}
+    </div>
   )
 }
 export default Spollers;
